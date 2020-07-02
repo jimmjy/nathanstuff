c514f7b5bbcf24447ddb91490fb74fd013be3af4
James Finkelstein <jfinkelstein9@me.com>
2020-07-02 17:23:56 -0400

    added some test text

--- src/pages/employment/employment.component.jsx
+++ src/pages/employment/employment.component.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const Employment = () => <div>
-    Employment Page
-</div>;
+const Employment = () => (
+	<div>
+		Employment Page: Do you have a good attitude? Are you friendly? A good
+		listener? Do messes, dirt, and disorganization bother you? Are you able to
+		take directions and execute them with proficiency? Do you feel a sense of
+		achievement and pride after doing a good job? Punctual? Are you honest and
+		trustworthy? Self-motivated? Do you want to work in a vibrant respectful
+		environment where you are rewarded fairly for your efforts? If your answer
+		is yes, then we want to hear from you. Call and speak to our friendly
+		Solutions Specialist. call use: number here
+	</div>
+);
 
 export default Employment;
