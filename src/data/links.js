948721b2e88fa255986b40e0b5e104bca120b94b
James Finkelstein <jfinkelstein9@me.com>
2021-02-22 18:40:32 -0500

    updated some small styling

--- src/data/links.js
+++ src/data/links.js
@@ -1,26 +1,26 @@
 export const navLinks = [
-	{
-		name: 'Home',
-		link: '/',
-	},
-	{
-		name: 'About',
-		link: '/about',
-	},
-	{
-		name: 'Services',
-		link: '/services',
-	},
-	{
-		name: 'Employement',
-		link: '/employment',
-	},
-	{
-		name: 'Testimonials',
-		link: '/testimonials',
-	},
-	{
-		name: 'Contact',
-		link: '/contact',
-	},
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "/about",
+  },
+  {
+    name: "Services",
+    link: "/services",
+  },
+  {
+    name: "Employment",
+    link: "/employment",
+  },
+  {
+    name: "Testimonials",
+    link: "/testimonials",
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+  },
 ];
