a73720e82c676eea93a9a46d543ea06a5128fa88
James Finkelstein <jfinkelstein9@me.com>
2021-02-22 18:26:37 -0500

    updated content

--- src/pages/employment/employment.component.jsx
+++ src/pages/employment/employment.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 // data
 import { employmentInfo } from "../../data/employment";
