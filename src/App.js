2c62bee7b7368e95ebeda1be296230e115a94f25
James Finkelstein <jfinkelstein9@me.com>
2021-02-22 10:42:42 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { withAuthentication } from "./components/sessions";
 
 //components
-import {HomePage, About, Contact, Employment, Services, Admin, AdminConsole, ForgotPassword, Testimonials} from "./pages";
+import {
+  HomePage,
+  About,
+  Contact,
+  Employment,
+  Services,
+  Admin,
+  AdminConsole,
+  ForgotPassword,
+  Testimonials,
+} from "./pages";
 import NavBar from "./components/navigation/navigation.component";
 import Footer from "./components/footer/footer.component";
 import Spacer from "./components/spacer/spacer.component";
-
 
 const App = ({ firebase: { doSignOut } }) => {
   useEffect(() => {
@@ -24,16 +33,16 @@ const App = ({ firebase: { doSignOut } }) => {
         <NavBar />
         <Spacer />
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/employment" component={Employment} />
-          <Route path="/services" component={Services} />
-          <Route path="/admin" component={Admin} />
-          <Route path="/testimonials" component={Testimonials} />
-          <Route path="/console" component={AdminConsole} />
-          <Route path="/reset" component={ForgotPassword} />
-          <Route path="/" component={Error} />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/employment' component={Employment} />
+          <Route path='/services' component={Services} />
+          <Route path='/admin' component={Admin} />
+          <Route path='/testimonials' component={Testimonials} />
+          <Route path='/console' component={AdminConsole} />
+          <Route path='/reset' component={ForgotPassword} />
+          <Route path='/' component={Error} />
         </Switch>
         <Footer />
       </>
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,38 +3,39 @@ import "firebase/auth";
 import "firebase/database";
 
 const config = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_DATABASE_URL,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 class Firebase {
-	constructor() {
-		app.initializeApp(config);
+  constructor() {
+    app.initializeApp(config);
 
-		this.auth = app.auth();
-		this.db = app.database();
-	}
+    this.auth = app.auth();
+    this.db = app.database();
+  }
 
-	// *** Auth API ***
-	doCreateUserWithEmailAndPassword = (email, password) =>
-		this.auth.createUserWithEmailAndPassword(email, password);
+  // *** Auth API ***
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
 
-	doSignInWithEmailAndPassword = (email, password) =>
-		this.auth.signInWithEmailAndPassword(email, password);
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
 
-	doSignOut = () => this.auth.signOut();
+  doSignOut = () => this.auth.signOut();
 
-	doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
 
-	doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+  doPasswordUpdate = (password) =>
+    this.auth.currentUser.updatePassword(password);
 
-	//*** message/ API ***
-	message = uid => this.db.ref(`messages/${uid}`);
-	messages = () => this.db.ref("/messages");
+  //*** message/ API ***
+  message = (uid) => this.db.ref(`messages/${uid}`);
+  messages = () => this.db.ref("/messages");
 }
 
 export default Firebase;
